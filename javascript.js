8dc2029ffb2c3a28b3aca86beecd61911e3a1e20
Vishalpandey1799 <130959006+Vishalpandey1799@users.noreply.github.com>
2024-06-03 16:21:13 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,70 @@
+
+
+let leftClick = document.querySelector(".left");
+let rightClick = document.querySelector(".right");
+let copytoClipboard = document.querySelector(".gradiantCopy");
+let body = document.querySelector("body");
+let inputBox = document.querySelector("input");
+let visible = document.querySelector(".sec");
+let clickBtn = document.querySelector(".btn");
+let formed = document.querySelector(".formed");
+ 
+
+
+const gradiantColors = () =>{
+    let hexcode = "0123456789abcdef"
+    let addhash = "#"
+    for(let i = 0; i<6; i++){
+       addhash = addhash+hexcode[Math.floor(Math.random()*16)];
+    }
+
+    return addhash;
+    
+}
+
+
+// console.log(gradiantColors());
+let colorCode = "#5474a7";
+let colorcode = "#784245";
+let gradiantMix = () =>{
+     colorCode = gradiantColors();
+    console.log(colorCode);
+    leftClick.textContent = `${colorCode}`
+    leftClick.style.backgroundColor = `${colorCode}`;
+    body.style.backgroundImage = `linear-gradient(to right , ${colorCode} , ${colorcode})`;
+    
+}
+let gradiantmix = () =>{
+    colorcode = gradiantColors();
+    console.log(colorcode);
+    rightClick.textContent = `${colorcode}`
+    rightClick.style.backgroundColor = `${colorcode}`;
+
+    body.style.backgroundImage = `linear-gradient(to right , ${colorCode} , ${colorcode})`;
+
+
+}
+ 
+
+leftClick.addEventListener("click", gradiantMix);
+rightClick.addEventListener("click", gradiantmix);
+
+clickBtn.addEventListener("click" , (e) =>{
+    e.preventDefault();
+    console.log("clicked");
+    if(inputBox.value === "yes"){
+        visible.style.display = "flex";
+        formed.style.display = "none";
+
+    }else{
+        alert("Kya malik ! Sachiv Bniyega ?");
+    }
+});
+
+
+copytoClipboard.addEventListener("click" , () =>{
+    navigator.clipboard.writeText(copytoClipboard.textContent);
+    alert("copied !");
+});
+
+    
